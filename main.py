4fa69a2128069297026deeb9e8c1be37d52534d9
vladkuz54 <vkuz1540@gmail.com>
2025-04-14 21:43:12 +0300

    added a model

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+#
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
+#
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(10, activation='softmax'))
+#
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#
+# model.fit(x_train, y_train, epochs=6)
+#
+# model.save('handwritten.h5')
+
+model = tf.keras.models.load_model('handwritten.h5')
+
+
+image_num = 1
+while os.path.isfile(f'digits/digit{image_num}.png'):
+    try:
+        img = cv2.imread(f'digits/digit{image_num}.png')[:, :, 0]
+        img = np.invert(np.array([img]))
+        pred = model.predict(img)
+        print(f"This digit{image_num} is probably a {np.argmax(pred)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print('Error!')
+    finally:
+        image_num += 1
